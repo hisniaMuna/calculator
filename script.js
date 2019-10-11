afa7965a12b4a2b5f1efcac6a603e50ceabf11aa
hisnia <hisniamuna12@gmail.com>
2019-10-11 12:06:47 +0700

    hitung

---
+++ script.js
@@ -0,0 +1,36 @@
+alert('Thanks For Visit');
+
+function input(e)
+{
+    val=document.myForm.textView.value
+    if(e)
+    document.myForm.textView.value+=e;
+    else
+    document.myForm.textView.value=null;
+}
+
+function output()
+{
+try{
+    val=document.myForm.textView.value
+    if(val)
+{
+    var output=eval(document.myForm.textView.value);
+    return document.myForm.textView.value=output;
+        }
+    }
+
+
+catch(e)
+{
+    alert(e.messege);
+    //document.myForm.textView.value=null;
+}
+}
+function bsp()
+{
+    val=document.myForm.textView.value;
+    back=val.substring(0,val.length-1)
+
+    document.myForm.textView.value=back;
+}
